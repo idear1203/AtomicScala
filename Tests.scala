0ca47a665db1d493c9a274ec6b309e438553fc24
idear <wangdongwei@pku.edu.cn>
2016-10-20 15:47:32 +0800

    测试

---
+++ Tests.scala
@@ -0,0 +1,26 @@
+// Tests.scala
+
+import com.atomicscala.AtomicTest._
+
+// 1.
+val myValue1 = 20
+val myValue2 = 10
+
+myValue1 is myValue2
+
+// 2.
+val myValue3 = 10
+val myValue4 = 10
+
+myValue3 is myValue4
+
+// 3.
+myValue2 is myValue3
+
+// 4.
+val myValue5 = "10"
+
+// Matched, this test framework is string based.
+myValue2 is myValue5
+
+// 5.
