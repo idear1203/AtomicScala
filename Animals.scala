343e01374bbcb4f1c1e8a05380c338b4a7ed9c4b
idear <wangdongwei@pku.edu.cn>
2016-10-20 14:24:02 +0800

    构建类

---
+++ Animals.scala
@@ -0,0 +1,37 @@
+// Animal.scala
+
+// 1.
+class Hippo
+class Lion
+class Tiger
+class Monkey
+class Giraffe
+
+val hippo = new Hippo
+val lion = new Lion
+val tiger = new Tiger
+val monkey = new Monkey
+val giraffe = new Giraffe
+
+println(hippo)
+println(lion)
+println(tiger)
+println(monkey)
+println(giraffe)
+
+// 2.
+val lion2 = new Lion
+val giraffe2 = new Giraffe
+val giraffe3 = new Giraffe
+
+println(lion2)
+println(giraffe2)
+println(giraffe3)
+
+// 3.
+class Zebra {
+    println("I have stripes")
+}
+
+val zebra = new Zebra
+println(zebra)
